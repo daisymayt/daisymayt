fca71ac6da49b3e200ba68a6007f760357553111
Daisy-May Ticehurst <152504775+daisymayt@users.noreply.github.com>
2024-04-24 21:04:12 +0100

    Lock window movement (#1)
    * Create UI Canvas
    
    * Freeze window on button click

---
+++ UnityDissertation/Assets/staticToggle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class staticToggle : MonoBehaviour
+{
+    public Button toggleStaticButton;
+    public GameObject grab;
+    public GameObject distanceGrab;
+    private bool doubleClick = false;
+
+    void Start()
+    {
+        Button btn = toggleStaticButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        if (doubleClick)
+        {
+            distanceGrab.SetActive(!distanceGrab.activeSelf);
+            grab.SetActive(!grab.activeSelf);
+            doubleClick = false;
+
+            var currentColor = GetComponent<Button>().image.color;
+            if (currentColor == Color.gray)
+            {
+                GetComponent<Button>().image.color = Color.white;
+            }
+            else
+            {
+                GetComponent<Button>().image.color = Color.gray;
+            }
+        }
+        else
+        {
+            doubleClick = true;
+        }
+    }
+}
